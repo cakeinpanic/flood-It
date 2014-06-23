cadf604239f3b5c499c8689cbabe4e337d19dce2
Katerina <cakeinpanic@gmail.com>
2014-06-23 23:57:10 +0400

    meow

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ var	colorScheme1 		=[["#57385C", "#A75265", "#EC7263", "#FEBE7E","#FFEDBC"],
 
 
 window.onload=function(){
-	alert(window.width)
+
 	var $wrapper= $('.wrapper')[0];
 	var a=new Game(12,colorScheme1);
 	a.startGame();
 	$wrapper.classList.remove("d")
+	alert($(window).width())
 }
